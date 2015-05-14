586925e03e2229f3546e475e24808a6ef1076080
Garrett Wollman <wollman@csail.mit.edu>
2015-05-14 17:26:00 -0400

    bind::package: implement

---
+++ manifests/package.pp
@@ -0,0 +1,14 @@
+#
+# Install the BIND server package(s) and, if desired, documentation.
+#
+class bind::package ($ensure, $packages, $install_documentation,
+		     $documentation_packages) {
+  package { $packages:
+    ensure => $ensure,
+  }
+  if ($install_documentation and !empty($documentation_packages)) {
+    package { $documentation_packages:
+      ensure => $ensure,
+    }
+  }
+}
