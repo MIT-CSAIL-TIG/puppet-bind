6467bd217f75c15ed4ea31b253fc3eff180ae0c5
Garrett Wollman <wollman@csail.mit.edu>
2015-05-14 15:52:54 -0400

    bind::tools: initial implementation
    Add initial support for installing the BIND tools.  The package names
    vary from OS to OS, and there's the additional complication that the
    bind-tools package on some OSes conflicts with the bind-server
    package, so the bind::tools class has to be told whether or not the
    server is being installed.  For this reason, the bind::tools class
    should be considered an internal implementation detail of the bind
    class.
    
    Right now the bind class can't do anything *but* install the tools.

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+#
+# Main class to install and manage ISC BIND.  Written for and tested on
+# BIND 9.10, but other versions may also work.
+#
+# If we're not going to be installing the BIND server, but just the tools,
+# then don't try to do anything else.  This is because on some systems
+# (e.g., FreeBSD), the bind-server package includes the tools and conflicts
+# with the bind-tools package.  If installing the server, make sure that
+# bind::tools knows so it doesn't try to install a package that conflicts
+# with the server package.
+#
+# A site that wants to have bind-tools installed everywhere can simply set
+# bind::tools_only to true in Hiera data, then override this setting in
+# the machine classes that will actually run servers.
+#
+class bind ($tools_only) {
+  if ($tools_only) {
+    anchor { 'bind::begin': } -> 
+    class { 'bind::tools': installing_server => false, } ->
+    anchor { 'bind::end': }
+  } else {
+    anchor { 'bind::begin':
+    } -> class { 'bind::package':
+    } -> class { 'bind::tools':
+      installing_server => true,
+    } -> class { 'bind::config':
+    } -> class { 'bind::service':
+    } -> anchor { 'bind::end':
+    }
+  }
+}
---
+++ manifests/tools.pp
@@ -0,0 +1,23 @@
+#
+# Install the BIND tools, which may or may not be in a separate
+# package from BIND itself.  One challenge: if there are two packages,
+# they may conflict!  So we have to gate everything through the
+# main 'bind' class, which knows whether the server will be installed
+# or not, and which will instantiate this class if and only if required.
+# For now, all this does is ensure the package, but it's broken out this
+# way in case there is ever some special configuration that needs to be
+# done.
+#
+class bind::tools ($ensure, $packages, $installing_server,
+		   $tools_conflict_with_server) {
+  if ($ensure == 'present' or $ensure == 'latest') {
+    if ($tools_conflict_with_server and $installing_server) {
+      # do nothing
+      anchor { 'bind::tools::empty': }
+    } else {
+      package { $packages: ensure => $ensure, }
+    }
+  } else {
+    package { $packages: ensure => $ensure, }
+  }
+}
