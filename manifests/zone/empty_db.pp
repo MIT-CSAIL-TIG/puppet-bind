2401724d2224bdcbcff3cec2d20734bc5fc31b78
Garrett Wollman <wollman@csail.mit.edu>
2015-07-20 15:30:21 -0400

    rename empty.db to managed-empty.db
    It's not a good idea to use the same name as the one in the BIND
    package, which is not considered a "configuration" file and thus
    causes checksum-mismatch alerts.

--- manifests/zone/empty_db.pp
+++ manifests/zone/empty_db.pp
@@ -1,11 +1,11 @@
 class bind::zone::empty_db ($ensure = 'present') {
   include bind::config
-  $db = "${bind::config::master_path}/empty.db"
+  $db = "${bind::config::master_path}/managed-empty.db"
   file {$db:
     ensure => $ensure,
     owner  => root,
     group  => '0',
-    source => 'puppet:///modules/bind/empty.db',
+    source => 'puppet:///modules/bind/managed-empty.db',
     mode   => '0444',
   }
 }
