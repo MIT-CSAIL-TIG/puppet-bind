9820d0737bce4fef6eb12dfa58b9df6f5d73f0ce
Garrett Wollman <wollman@csail.mit.edu>
2015-07-23 17:41:48 -0400

    bind::tsig_keys: write auto-generated warning to file

--- manifests/tsig_keys.pp
+++ manifests/tsig_keys.pp
@@ -1,3 +1,9 @@
+#
+# Instantiate the concat resource for the keys_dir/tsig.keys file and
+# arranges for named.conf to include the generated file.  In a separate
+# class so that it will only be generated/included if there are
+# bind::tsig resources that require it.
+#
 class bind::tsig_keys ($ensure = 'present') {
   include bind::config
 
@@ -5,8 +11,9 @@ class bind::tsig_keys ($ensure = 'present') {
   concat { $tsig_keys:
     ensure => $ensure,
     owner  => root,
-    group  => "0",
-    mode   => "0444",
+    group  => '0',
+    mode   => '0444',
+    warn   => true,
   }
   if $bind::config::checkconf {
     Concat[$tsig_keys] { validate_cmd => "${bind::config::checkconf} %", }
