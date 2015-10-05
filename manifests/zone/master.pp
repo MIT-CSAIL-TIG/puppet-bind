0da4e83abc7b9b769add19067329058326142e63
Garrett Wollman <wollman@csail.mit.edu>
2015-10-05 19:34:45 -0400

    zone::master: find keys and zone files in the right places; lint

--- manifests/zone/master.pp
+++ manifests/zone/master.pp
@@ -1,16 +1,27 @@
-define bind::zone::master ($zone = $name, $view, $source, 
+#
+# Instantiate a single zone as a master, in a specific view, using the
+# indicated souce file, optionally with inline DNSSec signing.
+#
+define bind::zone::master ($zone = $name, $view, $source,
 			   $inline_signing = false,
 			   $allow_transfer = undef, $notify_ = undef,
 			   $also_notify = undef,
 			   ) {
   include bind::config
-  $key_directory = $bind::config::key_directory
+  $key_directory = $bind::config::keys_path
+  $master_directory = $bind::config::master_path
 
-  bind::zone::generic {"$view/$zone":
+  if $source =~ /^\// {
+    $source_path = $source
+  } else {
+    $source_path = "${master_directory}/${source}"
+  }
+
+  bind::zone::generic {"${view}/${zone}":
     zone           => $zone,
     view           => $view,
     zone_type      => master,
-    zone_file      => $source,
+    zone_file      => $source_path,
     allow_transfer => $allow_transfer,
     notify_        => $notify_,
     also_notify    => $also_notify,
